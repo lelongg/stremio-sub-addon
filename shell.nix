6fbc931529b48b243c1d8521d175d40fafa7fd91
Gérald Lelong <gerald.lelong@easymov.fr>
2020-12-06 19:59:56 +0100

    initial commit

---
+++ shell.nix
@@ -0,0 +1,14 @@
+let
+  mozilla = import (builtins.fetchTarball
+    "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz");
+  pkgs = import <nixpkgs> { overlays = [ mozilla ]; };
+in with pkgs;
+let
+  rust = (rustChannelOf {
+    channel = "1.48.0";
+    sha256 = "sha256:0b56h3gh577wv143ayp46fv832rlk8yrvm7zw1dfiivifsn7wfzg";
+  }).rust.override { extensions = [ "rust-src" ]; };
+in mkShell {
+  buildInputs = [ clang rust openssl pkgconfig dbus.dev ];
+  LIBCLANG_PATH = "${llvmPackages.libclang}/lib";
+}
